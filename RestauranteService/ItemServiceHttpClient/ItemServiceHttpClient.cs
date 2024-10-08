ed1f0c2ee16e08e0363abcb4ad98ebb3b58393f6
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-10-08 11:25:04 -0400

    feature: Sending http requests

---
+++ RestauranteService/ItemServiceHttpClient/ItemServiceHttpClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text;
+using RestauranteService.Dtos;
+
+namespace RestauranteService.ItemServiceHttpClient
+{
+    public class ItemServiceHttpClient : IItemServiceHttpClient
+    {
+
+        private readonly HttpClient _client;
+
+        public ItemServiceHttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public void EnviaRestauranteParaItemService(RestauranteReadDto readDto)
+        {
+            var conteudoHttp = new StringContent
+                    (
+                      JsonSerializer.Serialize(readDto),
+                        Encoding.UTF8,
+                        "application/json"
+                    );
+        }
+    }
+}
